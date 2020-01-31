63274e1daa6b26fdde34eef41c84c1d9229f8265
Ryan <ryan.fee625@gmail.com>
2020-01-31 10:54:43 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,29 @@
 output "instance_id" {
   description = "Instance ID"
-  value       = aws_instance.scalr.id
+  value       = aws_instance.mysql1.id
 }
 
 output "public_ip" {
   description = "Public IP"
-  value       = aws_instance.scalr.public_ip
+  value       = aws_instance.mysql1.public_ip
 }
 
 output "private_ip" {
   description = "Private IP"
-  value       = aws_instance.scalr.private_ip
+  value       = aws_instance.mysql1.private_ip
+}
+
+output "instance_id" {
+  description = "Instance ID"
+  value       = aws_instance.worker.id
+}
+
+output "public_ip" {
+  description = "Public IP"
+  value       = aws_instance.worker.public_ip
+}
+
+output "private_ip" {
+  description = "Private IP"
+  value       = aws_instance.worker.private_ip
 }
